8c477d352579f77ed88845a46831b38bada87966
pp2903 <priyampranshu@gmail.com>
2023-03-26 16:25:25 +0530

    First commit

---
+++ Marks_Pizzeria/baseApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views as base_views
+from django.contrib.auth import views
+urlpatterns = [
+    
+    path("",base_views.index,name="home-page"),
+    path("about/",base_views.about,name="about-page"),
+    path("order/",base_views.menu,name="menu-page"),
+    path("signup/",base_views.signUp,name="signup-page"),
+    path("login/",views.LoginView.as_view(template_name="baseApp/login.html"),name="login-page"),
+    path("logout/",views.LogoutView.as_view(),name="logout-page"),
+
+]
