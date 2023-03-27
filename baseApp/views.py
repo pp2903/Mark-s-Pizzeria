ad58f55a595c27c0643911b001088f1a0d1f77b3
pp2903 <priyampranshu@gmail.com>
2023-03-27 17:25:00 +0530

    First commit on br1

---
+++ baseApp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from .forms import UserRegistrationForm
+# Create your views here.
+def index(request):
+
+    return render(request, "baseApp/index.html")
+
+
+def about(request):    
+    return render(request,'baseApp/about.html')
+
+
+
+def menu(request):
+
+    return render(request, 'baseApp/order.html')
+
+
+
+def signUp(request):
+    context = {}
+    form = UserRegistrationForm
+    context["form"] = form
+    if(request.method=="POST"):
+        form = UserRegistrationForm(request.POST)
+        if(form.is_valid()):
+            form.save()
+    
+    return render(request,'baseApp/signUp.html',context = context)
+
+
+
+def login(request):
+    context=  {}
+    # form = 
+    return render(request,'baseApp/login.html')
+
+    
